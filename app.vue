4b7d0d622df2f51561b14c0e5797cb182e93a853
shashank shandilya <shashank@oneorigin.us>
2023-01-06 15:47:02 -0700

    nuxt 3 starter

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script></script>
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Example app with daisyUi + talwind and typescript
+  </h1>
+  <div class="p-4">
+    <button class="btn">Hello daisyUI</button>
+  </div>
+  <Icon name="uil:github" />
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#570DF8",
+          secondary: "#F000B8",
+          accent: "#37CDBE",
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
