45575fa8050fab58cd874e7fb9e4623c5ae4cebc
Shashank shandilya <shashank.v.shandilya@gmail.com>
2023-01-06 17:34:21 -0700

    feat: added error page

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+  <template v-if="error.statusCode === 404 || '404'">
+    <p>[template]: Oops page not available 🥺</p>
+  </template>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: Object,
+});
+const error = useError();
+if (error.value.statusCode === 404 || "404") {
+  error.value.message = "[script]: Oops! Page not found 😔";
+}
+</script>
